0494a9319fd6aaba41a1cc16cd3c9adc3cdb0d58
Michał Jezierski <ambinanitelo@gmail.com>
2017-03-22 13:28:40 +0100

    contacting upm and installing add-on

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const got = require('got');
+
+async function getToken({ username, password, url }) {
+    const response = await got(`${url}?os_authType=basic`, {
+        auth: `${username}:${password}`,
+        headers: { Accept: 'application/vnd.atl.plugins.installed+json' }
+    });
+
+    return response.headers['upm-token'];
+}
+
+async function install({ username, password, host, descriptor }) {
+    const url = `https://${host}/rest/plugins/1.0/`;
+    const token = await getToken({ username, password, url });
+
+    const response = await got(`${url}?token=${token}`, {
+        auth: `${username}:${password}`,
+        method: 'POST',
+        json: true,
+        headers: { 'Content-Type': 'application/vnd.atl.plugins.remote.install+json' },
+        body: JSON.stringify({ pluginUri: descriptor })
+    });
+
+    return response;
+}
+
+module.exports = install;
